51147d7a63b6caf558a1ea23020316de11fade72
PeschykhinLev <91117670+PeschykhinLev@users.noreply.github.com>
2023-01-09 11:34:41 +0200

    Adding explanation

--- framework/src/main/java/org/fpm/di/DummyBinder.java
+++ framework/src/main/java/org/fpm/di/DummyBinder.java
@@ -12,6 +12,8 @@ import java.util.Map;
 public class DummyBinder implements Binder {
     private Map<Class<?>, Object> data = new HashMap<>();
     private Map<Class<?>, Class<?>> classData = new HashMap<>();
+    
+    // метод для того, щоб закинути клас у базу даних
     @Override
     public <T> void bind(Class<T> clazz) {
         if (clazz.isAnnotationPresent(Singleton.class)) {
@@ -21,7 +23,8 @@ public class DummyBinder implements Binder {
         data.put(clazz, null);
 
     }
-
+    
+    //метод для створення екземпляру класу з пустого конструктору
     private <T> T createInstanceOfClassT(Class<T> clazz){
         try {
             return clazz.getConstructor().newInstance();
@@ -29,23 +32,25 @@ public class DummyBinder implements Binder {
             throw new RuntimeException(e);
         }
     }
-
-
-
+    
+    //метод щоб закинути у базу даних injection graph
     @Override
     public <T> void bind(Class<T> clazz, Class<? extends T> implementation) {
         classData.put(clazz,implementation);
     }
-
+    
+    //метод для того, щоб закинути у базу даних імітацію singleton
     @Override
     public <T> void bind(Class<T> clazz, T instance) {
         data.put(clazz,instance);
     }
 
+    //метод для того, щоб спробувати дістати injection graph
     public Class<?> getClassData(Class<?> clazz) {
         return classData.getOrDefault(clazz, null);
     }
 
+    //метод для того, щоб спробувати дістати клас, або імітацію singleton
     public Object getData(Class<?> clazz){
         return data.getOrDefault(clazz, null);
     }
